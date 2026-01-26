f72b259ee1ae370989452131c5a82e922f7921f4
Ephemera <kwanghaualex24s@forward.edu.my>
2026-01-26 10:21:04 +0800

    Made LoginScreen.kt remove itself when navigating to Home. Updated both LoginScreen.kt and RegisterScreen.kt to not use FieldData.
    Updated BreakdownCard.kt for HomeScreen to use enum classes instead of Strings and Ints as dataType. Made the ExpenseCard.kt not show when there is no data to show

--- app/src/main/java/com/fwrdgrp/financetracker/ui/composables/home/BreakdownCard.kt
+++ app/src/main/java/com/fwrdgrp/financetracker/ui/composables/home/BreakdownCard.kt
@@ -1,36 +1,35 @@
 package com.fwrdgrp.financetracker.ui.composables.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.fwrdgrp.financetracker.data.enum.DateFilter
 import com.fwrdgrp.financetracker.data.model.ui.PieChartData
+import com.fwrdgrp.financetracker.ui.composables.charts.PieChart
+import com.fwrdgrp.financetracker.ui.composables.general.TabButtons
 
 @Composable
 fun BreakdownCard(
-    tabs: List<String>,
+    tabs: List<DateFilter>,
     pieData: List<PieChartData>,
-    selectedTab: Int,
-    onTabSelect: (Int) -> Unit
+    selectedTab: DateFilter,
+    onTabSelect: (DateFilter) -> Unit
 ) {
     Box(
         modifier = Modifier
@@ -45,51 +44,58 @@ fun BreakdownCard(
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            if (pieData.isNotEmpty()) {
 
-            Text(
-                "Spending Breakdown - Month",
-                fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
-            )
-            Spacer(Modifier.height(8.dp))
-            HorizontalDivider(thickness = 1.dp)
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                tabs.forEachIndexed { index, tab ->
-                    Box(
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxSize()
-                            .background(
-                                color = if (selectedTab == index) Color.Black
-                                else Color.White
-                            )
-                            .clickable { onTabSelect(index) },
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = tab,
-                            fontSize = 14.sp,
-                            color = if (selectedTab == index) Color.White
-                            else Color.LightGray
-                        )
-                    }
-                    if (index < 2) {
-                        VerticalDivider(thickness = 1.dp, modifier = Modifier.height(48.dp))
-                    }
-                }
+                Text(
+                    "Spending Breakdown - Month",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                )
+                Spacer(Modifier.height(8.dp))
+                HorizontalDivider(thickness = 1.dp)
+                TabButtons(tabs, selectedTab, { it.name })
+                { onTabSelect(it) }
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .height(48.dp),
+//                    horizontalArrangement = Arrangement.SpaceEvenly
+//                ) {
+//                    tabs.forEachIndexed { index, tab ->
+//                        Box(
+//                            modifier = Modifier
+//                                .weight(1f)
+//                                .fillMaxSize()
+//                                .background(
+//                                    color = if (selectedTab == index) Color.Black
+//                                    else Color.White
+//                                )
+//                                .clickable { onTabSelect(index) },
+//                            contentAlignment = Alignment.Center
+//                        ) {
+//                            Text(
+//                                text = tab,
+//                                fontSize = 14.sp,
+//                                color = if (selectedTab == index) Color.White
+//                                else Color.Black
+//                            )
+//                        }
+//                        if (index < tabs.size - 1) {
+//                            VerticalDivider(thickness = 1.dp, modifier = Modifier.height(48.dp))
+//                        }
+//                    }
+//                }
+                HorizontalDivider(thickness = 1.dp)
+                PieChart(
+                    data = pieData,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            } else {
+                Text("Add a transaction", textAlign = TextAlign.Center)
+                Text("to see the breakdown", textAlign = TextAlign.Center)
             }
-            HorizontalDivider(thickness = 1.dp)
-            PieChart(
-                data = pieData,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            )
         }
     }
 }
